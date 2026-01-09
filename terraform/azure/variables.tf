7451ded107b99ef0d04d0a0adc89eb84e3ac508d
shravank121 <shravankulkarni65@gmail.com>
2026-01-09 16:53:13 +0530

    feat(terraform): add Azure infrastructure with Container Apps and Key Vault

---
+++ terraform/azure/variables.tf
@@ -0,0 +1,50 @@
+variable "azure_region" {
+  description = "Azure region"
+  type        = string
+  default     = "eastus"
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "devops"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "prod"
+}
+
+variable "vnet_cidr" {
+  description = "VNet CIDR block"
+  type        = string
+  default     = "10.1.0.0/16"
+}
+
+variable "backend_image" {
+  description = "Backend Docker image"
+  type        = string
+}
+
+variable "frontend_image" {
+  description = "Frontend Docker image"
+  type        = string
+}
+
+variable "acr_login_server" {
+  description = "Azure Container Registry login server"
+  type        = string
+  default     = "devopsacrshravan.azurecr.io"
+}
+
+variable "acr_username" {
+  description = "Azure Container Registry username"
+  type        = string
+}
+
+variable "acr_password" {
+  description = "Azure Container Registry password"
+  type        = string
+  sensitive   = true
+}
